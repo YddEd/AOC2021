a0891bb0f9fe85adafc6d4b393a03a7347781b06
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-03 02:55:55 +1100

    add part two solution

--- 2/sol.py
+++ 2/sol.py
@@ -1,6 +1,7 @@
 def part_one(input_file):
     horizontal = 0
     depth = 0
+
     with open(input_file, "r") as f:
         input = f.readlines()
         for line in input:
@@ -15,4 +16,27 @@ def part_one(input_file):
 
     return horizontal * depth
 
+def part_two(input_file):
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    with open(input_file, "r") as f:
+        input = f.readlines()
+        for line in input:
+            direction = line.split()[0]
+            units = int(line.split()[1])
+            if direction == "forward":
+                horizontal += units
+                depth += aim * units
+            elif direction == "down":
+                aim += units
+            elif direction == "up":
+                aim -= units
+
+    return horizontal * depth
+
+print(part_one("sample.txt"))
 print(part_one("input.txt"))
+print(part_two("sample.txt"))
+print(part_two("input.txt"))
