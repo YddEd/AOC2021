8f01e4c425d269cab9d32c79de5fcb10ce892d37
esha7130 <esha7130@uni.sydney.edu.au>
2021-12-03 02:24:40 +1100

    add solution for part two

--- 1/sol.py
+++ 1/sol.py
@@ -1,9 +1,23 @@
-measurements = 0
-with open("input.txt", "r") as f:
-    input = f.readlines()
-    i = 1
-    while i < len(input):
-        if int(input[i]) > int(input[i-1]):
-            measurements += 1
-        i += 1
-print(measurements)
+def part_one(input_file):
+    measurements = 0
+    with open(input_file, "r") as f:
+        input = f.readlines()
+        i = 1
+        while i < len(input):
+            if int(input[i]) > int(input[i-1]):
+                measurements += 1
+            i += 1
+    return measurements
+
+def part_two(input_file):
+    counter = 0
+    with open(input_file, "r") as f:
+        input = f.readlines()
+        i = 3
+        while i < len(input):
+            first_window = int(input[i-3]) + int(input[i-2]) + int(input[i-1])
+            second_window = int(input[i-2]) + int(input[i-1]) + int(input[i])
+            if second_window > first_window:
+                counter += 1
+            i += 1
+    return counter
